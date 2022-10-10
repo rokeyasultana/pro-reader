c85330eaea9c8b9edb3fb4f61d62aa2e469b74c6
jakiasultana2022 <rokeyasbd@gmail.com>
2022-10-10 16:18:44 +0600

    project initial setup

---
+++ src/components/Home/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+
+
+const Home = () => {
+    return (
+        <div>
+    <div className="hero min-h-screen ">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+  
+    <div>
+      <h1 className="text-5xl font-bold">A <span className='text-blue-500'>reader</span> lives a <br></br> thousand lives <span className='text-blue-500'>before he dies</span></h1>
+      <p className="py-6 text-left">Of all the skills you’ll ever learn growing up, arguably none are more important than the ability to read. Each book that you read is an opportunity to learn something new. You can expand your vocabulary, train your brain, increase wellness, lower stress, and discover something new about yourself.
+
+I still find the human mind amazing. You can see what is ultimately a bunch of scribbles on a page, decipher them into sounds, turn those sounds into words, and then turn those words into thoughts and emotions.</p>
+      <button className="btn btn-outline btn-info">Visit Store</button>
+    </div>
+    <Player 
+src='https://assets3.lottiefiles.com/packages/lf20_bnxoocv2.json'
+className="player"
+loop
+autoplay
+style={{ height: '600px', width: '600px' }}
+/>
+<Controls visible={true} buttons={['play', 'repeat', 'frame', 'debug']} />
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Home;
